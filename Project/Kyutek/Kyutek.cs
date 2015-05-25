5694f89415163ff59e1fe2ab467959f14268ed9b
darighteous1 <tonislav.atanasov@gmail.com>
2015-05-25 16:16:46 +0300

    - added story files and audio files - added project solution - reorganized directories - added classes

---
+++ Project/Kyutek/Kyutek.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kyutek
+{
+    class Kyutek
+    {
+        static void Main(string[] args)
+        {
+            // intro
+
+            // story - 1 (introduction)
+            
+            // choose a name
+
+            // story - 2 (prequel)
+
+            // choose a character class
+
+            // story - 3 (going to the bar, first interaction) 
+            
+            // first battle
+
+            // story - 4
+
+            // second battle
+
+            // story - 5
+
+            // third battle
+
+            // story 6
+
+            // final battle
+
+            // outro
+
+            // credits
+        }
+    }
+}
---
+++ old_files/TeamWork/SoundAndText/SoundAndText.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Media;
+
+namespace BitBuilderNew
+{
+    class BitBuilderNew
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            string textPath = @"E:\Fundamentials\TeamWork\text.txt";
+            string backGround = @"E:\Fundamentials\TeamWork\Star_Wars.wav";
+            SoundPlayer backGroundsong = new SoundPlayer(backGround);
+            backGroundsong.Play();
+
+            string logoPath = @"E:\Fundamentials\TeamWork\logo.txt";
+            string[] logo = File.ReadAllLines(logoPath);
+            Console.WriteLine();
+            foreach (string item in logo)
+            {
+                Console.Write(new string(' ', (Console.WindowWidth - item.Length) / 2));
+                Console.WriteLine(item);
+                Thread.Sleep(1000);
+            }
+            Thread.Sleep(3000);
+            Console.Clear();
+            backGroundsong.Dispose();
+
+            PrintTypewriterStyle(textPath);            
+        }
+ 
+        static void PrintTypewriterStyle(string textPath)
+        {
+            string soundPath = @"E:\Fundamentials\TeamWork\typewriter-1.wav";
+            string[] text = File.ReadAllLines(textPath);
+
+            SoundPlayer typewriter = new SoundPlayer(soundPath);            
+            typewriter.LoadAsync();
+
+            foreach (string item in text)
+            {
+                typewriter.Play();
+                foreach (char ch in item)
+                {
+                    Thread.Sleep(100);
+                    Console.Write(ch);
+                }
+                typewriter.Stop();
+                Thread.Sleep(1000);
+                Console.Clear();
+            }
+            typewriter.Dispose();
+        }
+    }
+}
